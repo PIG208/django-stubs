d929730e8b7f78e8c4c0947b30be6a2d877d3fc8
Zixuan James Li <p359101898@gmail.com>
2022-07-05 21:27:42 -0400

    Reflect the deprecation of get_response being None.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- django-stubs/views/decorators/csrf.pyi
+++ django-stubs/views/decorators/csrf.pyi
@@ -9,7 +9,6 @@ class _EnsureCsrfToken(CsrfViewMiddleware): ...
 requires_csrf_token: Any
 
 class _EnsureCsrfCookie(CsrfViewMiddleware):
-    get_response: None
     def process_view(self, request: Any, callback: Any, callback_args: Any, callback_kwargs: Any): ...
 
 ensure_csrf_cookie: Any
